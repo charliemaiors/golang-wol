6d3ef42c08ed69ebd4deee2fe2d907fbf53abe53
carlo.maiorano <pasqualecarlo.maiorano@yahoo.it>
2017-10-02 17:57:07 +0200

    handling

--- storage/handler.go
+++ storage/handler.go
@@ -1,9 +1,20 @@
 package storage
 
 import (
-	"log"
+	"bytes"
+	hash "crypto/sha512"
+	"encoding/gob"
+	"strings"
 
 	storage "github.com/coreos/bbolt"
+	log "github.com/sirupsen/logrus"
+	"masinihouse.ddns.net/git/charliemaiors/Golang-Wol/types"
+)
+
+const (
+	devicesBucket  = "DevBucket"
+	passwordBucket = "PassBucket"
+	passworkdKey   = "AdminPassword"
 )
 
 var db *storage.DB
@@ -14,7 +25,116 @@ func init() {
 		log.Fatal(err)
 	}
 
+	log.Debugf("Openend database %v, starting bucket definition", localDB)
 	db = localDB
+
+	err = db.Update(func(transaction *storage.Tx) error {
+		if _, createErr := transaction.CreateBucketIfNotExists([]byte(devicesBucket)); createErr != nil {
+			log.Errorf("Error creating devicesBucket: %v", createErr)
+			return createErr
+		}
+		if _, createErr := transaction.CreateBucketIfNotExists([]byte(passwordBucket)); createErr != nil {
+			log.Errorf("Error creating passwordBucket: %v", createErr)
+			return createErr
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Errorf("Got erro %v, panic!!!", err)
+		panic(err)
+	}
 }
 
-func StartHandling()
+func StartHandling(deviceChan chan *types.Alias, getChan chan *types.GetDev) {
+
+	for {
+		select {
+		case newDev := <-deviceChan:
+			log.Debugf("%v", newDev)
+		case getDev := <-getChan:
+			log.Debug("%v", getDev)
+		}
+	}
+}
+
+func addDevice(device *types.Device, name string) error {
+	log.Debugf("Adding device %v with name %s", device, name)
+	buf, err := encodeFromMacIface(device.Mac, device.Iface)
+
+	if err != nil {
+		log.Errorf("Got error encoding: %v", err)
+		return err
+	}
+
+	err = db.Update(func(transaction *storage.Tx) error {
+		bucket := transaction.Bucket([]byte(devicesBucket))
+		err := bucket.Put([]byte(name), buf.Bytes())
+		log.Debugf("Error? %v", err)
+		return err
+	})
+	return err
+}
+
+func getDevice(name string) (*types.Device, error) {
+	device := &types.Device{}
+	log.Debugf("Getting data for device with alias %s", name)
+
+	err := db.View(func(transaction *storage.Tx) error {
+		bucket := transaction.Bucket([]byte(devicesBucket))
+		dev := bucket.Get([]byte(name))
+		reader := bytes.NewReader(dev)
+		err := gob.NewDecoder(reader).Decode(&device)
+
+		if err != nil {
+			log.Errorf("Got error decoding: %v", err)
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	log.Debugf("Device is: %v", device)
+	return device, nil
+}
+
+func checkPassword(pass string) bool {
+	ok := false
+	passHash := hash.New()
+	effectiveHash := string(passHash.Sum([]byte(pass)))
+
+	db.View(func(transaction *storage.Tx) error {
+		bucket := transaction.Bucket([]byte(passwordBucket))
+		savedPass := bucket.Get([]byte(passworkdKey))
+		log.Debugf("Got %s for password from bucket", string(savedPass))
+		if strings.Compare(string(savedPass), effectiveHash) == 0 {
+			ok = true
+		}
+		return nil
+	})
+	return ok
+}
+
+func insertPassword(pass string) error {
+	passHash := hash.New()
+	effectiveHash := passHash.Sum([]byte(pass))
+
+	db.View(func(transaction *storage.Tx) error {
+		if 
+
+		bucket := transaction.Bucket([]byte(passwordBucket))
+		err := bucket.Put([]byte(passworkdKey), effectiveHash)
+		log.Errorf("Got %v for error", err)
+
+		return err
+	})
+}
+
+func encodeFromMacIface(mac, iface string) (*bytes.Buffer, error) {
+	buf := bytes.NewBuffer(nil)
+	entry := types.Device{Mac: mac, Iface: iface}
+	err := gob.NewEncoder(buf).Encode(entry)
+	return buf, err
+}
--- types/commons.go
+++ types/commons.go
@@ -4,3 +4,14 @@ type Device struct {
 	Mac   string `json:"mac"`
 	Iface string `json:"iface"`
 }
+
+type Alias struct {
+	Device   *Device
+	Name     string
+	Response chan struct{}
+}
+
+type GetDev struct {
+	Alias    string
+	Response chan *Device
+}
