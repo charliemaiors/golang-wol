a3a87cf9a07885f099ef19fffdeaba2faccb7c8d
Carlo Maiorano <charliemaiors@gmail.com>
2017-10-04 19:51:52 +0200

    redirecting

--- server/commons.go
+++ server/commons.go
@@ -2,15 +2,28 @@ package server
 
 import (
 	"errors"
+	"net/http"
 
 	"bitbucket.org/cmaiorano/golang-wol/types"
 	wol "github.com/sabhiram/go-wol"
 )
 
+var initialized = false
 var deviceChan = make(chan *types.Alias)
 var getChan = make(chan *types.GetDev)
 var passHandlingChan = make(chan *types.PasswordHandling)
 var updatePassChan = make(chan *types.PasswordUpdate)
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	if !initialized {
+		redirectToConfig(w, r)
+		return
+	}
+}
+
+func redirectToConfig(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/config", 301)
+}
 
 func registerDevice(alias, mac, iface string) error {
 	dev := &types.Device{Iface: iface, Mac: mac}
