63f568ca01c29fa73695aff69ce0c985395089b8
carlo.maiorano <pasqualecarlo.maiorano@yahoo.it>
2017-10-06 13:11:36 +0200

    added device, now rooting

--- server/commons.go
+++ server/commons.go
@@ -47,8 +47,15 @@ func handleDevices(w http.ResponseWriter, r *http.Request) {
 	}
 	switch r.Method {
 	case "GET":
-
+		templ, err := template.ParseFiles("templates/add-device.gohtml")
+		templ = template.Must(templ, err)
+		err = templ.Execute(w, ifaceList)
+		if err != nil {
+			panic(err)
+		}
 	case "POST":
+		handleDevicePost(w, r)
+		return
 	default:
 		handleError(w, r, errors.New("Not Allowed"), 405)
 	}
@@ -58,13 +65,18 @@ func redirectToConfig(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/config", 301)
 }
 
-func config(w http.ResponseWriter, r *http.Request) {
+func handleConfig(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET": //Got first request, sending back page
 		templ, err := template.ParseFiles("templates/config.html")
 		templ = template.Must(templ, err)
 		templ.Execute(w, nil)
 	case "POST": //Got submit running it!!!
+		err := r.ParseForm()
+		if err != nil {
+			handleError(w, r, err, 422)
+			return
+		}
 		password := r.FormValue("password")
 		if password == "" {
 			handleError(w, r, errors.New("Empty Password"), 422)
@@ -83,9 +95,35 @@ func config(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func registerDevice(alias, mac, iface string) error {
+func handleDevicePost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		handleError(w, r, err, 422)
+		return
+	}
+	respChan := make(chan error)
+	pass := &types.PasswordHandling{Password: r.FormValue("password"), Response: respChan}
+	passHandlingChan <- pass
+	err = <-respChan
+	if err != nil {
+		handleError(w, r, err, 401)
+		return
+	}
+
+	alias, regErr := registerDevice(r.FormValue("alias"), r.FormValue("macAddr"), r.FormValue("ifaces"))
+	if regErr != nil {
+		handleError(w, r, err, 422)
+		return
+	}
+
+	templ, err := template.ParseFiles("templates/add-device-success.gohtml")
+	templ = template.Must(templ, err)
+	templ.Execute(w, alias)
+}
+
+func registerDevice(alias, mac, iface string) (*types.Alias, error) {
 	if !reMAC.MatchString(mac) {
-		return errors.New("Invalid mac address format")
+		return nil, errors.New("Invalid mac address format")
 	}
 
 	dev := &types.Device{Iface: iface, Mac: mac}
@@ -94,9 +132,9 @@ func registerDevice(alias, mac, iface string) error {
 	deviceChan <- aliasStr
 
 	if _, ok := <-resp; !ok {
-		return errors.New("Error adding device")
+		return nil, errors.New("Error adding device")
 	}
-	return nil
+	return aliasStr, nil
 }
 
 func sendPacket(computerName, localIface string) error {
